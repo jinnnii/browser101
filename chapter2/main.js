86f18fa2b3607f47174ea342bf6ffb55aa453757
jinnnii <rdw_8690@naver.com>
2021-08-27 23:30:11 +0900

    Add Practice 7 : Coordinates Improvement

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,10 +1,13 @@
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const position = document.querySelector('.position');
+
+const targetSize = target.getBoundingClientRect();
+const targetWidth = targetSize.width / 2;
+const targetHeight = targetSize.height / 2;
+
 function updateDisplay(event) {
-
-    const horizontal = document.querySelector('.horizontal');
-    const vertical = document.querySelector('.vertical');
-    const tartget = document.querySelector('.target');
-    const position = document.querySelector('.position');
-
     const x = event.clientX;
     const y = event.clientY;
 
@@ -13,18 +16,23 @@ function updateDisplay(event) {
         Y : ${y}
     `);
 
-    vertical.style.left = `${x}px`;
+    // 1. Use all Operration 
+    /*vertical.style.left = `${x}px`;
     horizontal.style.top = `${y}px`;
 
-    tartget.style.top = `${y}px`;
-    tartget.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    target.style.left = `${x}px`;
 
     position.style.left = `${x}px`;
-    position.style.top = `${y}px`;
+    position.style.top = `${y}px`;*/
 
 
-
-
+    //2. Only use Composition
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetWidth}px,${y - targetHeight}px)`;
+    position.style.transform = `translate(${x}px,${y}px)`;
 }
 
-window.addEventListener("mousemove", (event) => updateDisplay(event), false);
+
+window.addEventListener("mousemove", (event) => updateDisplay(event));
